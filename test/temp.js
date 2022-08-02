168f8c27a31172f8f8f8320ea88d361fdaa32249
ypca1 <836908758@qq.com>
2022-08-02 17:51:30 +0800

    create temp

---
+++ test/temp.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         helloworld
+// @namespace    test
+// @version      0.1
+// @description  new script
+// @author       ypca1
+// @match        http://*/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    console.log('hello');
+})();
